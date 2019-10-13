3218592aafb5f23c41299647f37bbe7192a0487d
Echoic88 <caulfield.bd@outlook.com>
2019-10-13 19:22:59 +0100

    add requirements,.txt

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
-    print("hello")
+    return "hello"
 
 if __name__ == "__main__":
-    app.run(host = os.environ.get("IP"),
-            port = int(os.environ.get("PORT"),
-            debug = True)
+    app.run(host=os.getenv("IP"), port=(os.getenv("PORT")), debug=False)
