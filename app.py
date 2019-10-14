1a76cf6cb6ed236fb5f8dff6447a7cf418172320
Echoic88 <caulfield.bd@outlook.com>
2019-10-14 18:58:07 +0100

    Add ability to view and add  tasks.

--- app.py
+++ app.py
@@ -14,13 +14,21 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/add_task")
 def add_task():
-    return render_template("addtasks.html", categories=mongo.db.categories.find())
+    return render_template('addtask.html', categories=mongo.db.categories.find())
+
+
+@app.route('/insert_task', methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 @app.route("/get_tasks")
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+    
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"), port=(os.getenv("PORT")), debug=True)
